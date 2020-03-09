9ffbe67f5c5e9f36efef06f8861a899cef0bc713
jddeep <jddeep003@gmail.com>
2020-03-09 15:28:23 +0530

    updating with sample app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Sample App'),
+          backgroundColor: Colors.black,
+        ),
+      ),
+    ),
+  );
+}
